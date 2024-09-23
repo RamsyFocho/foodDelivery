c77bb729b9f112b959f7a6cc1c2971164568ec04
Ramsy <fochomukomr@gmail.com>
2024-09-23 12:09:15 +0100

    succeeding inserting records in the database and returning them as json objects

---
+++ src/main/java/com/newSpringBootProject/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.newSpringBootProject.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+//Data Access Layer
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+//create instances of the student object
+            Student ramsy = new Student(
+                        "Ramsy",
+                        "ramsy@gmail.com",
+                        LocalDate.of(2006, Month.FEBRUARY,10),
+                        21
+            );
+            Student alex= new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2003, Month.MAY,10),
+                    21
+            );
+//            saving the instances in the database
+            repository.saveAll(
+                    List.of(ramsy,alex)
+            );
+        };
+    }
+}
--- src/main/java/com/newSpringBootProject/demo/student/StudentController.java
+++ src/main/java/com/newSpringBootProject/demo/student/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+//api later
+
 @RestController //it will handle HTTP requests and return data (usually in JSON format).
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
